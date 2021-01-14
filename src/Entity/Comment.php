af5d42ef255f42512245a8233787a75ca5519bcd
Djioliat <alverbreugh@gmail.com>
2021-01-14 16:22:25 +0100

    Installation du projet et mise en place de la bdd

---
+++ src/Entity/Comment.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $commantaire;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommantaire(): ?string
+    {
+        return $this->commantaire;
+    }
+
+    public function setCommantaire(string $commantaire): self
+    {
+        $this->commantaire = $commantaire;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
